779f86dc90f33708582d83d7fbd2e82ace6ca0fd
Yasmin Carolina <carolinazanini305@gmail.com>
2024-09-11 17:25:45 -0300

    aula 03

---
+++ 09.js
@@ -0,0 +1,67 @@
+/*1. Crie uma função que adiciona um elemento ao final de um array.*/
+
+let array = [1,2,3]
+const manipulacaoArra = (valor) => {
+ array.push(valor) 
+ console.log (array)
+}
+
+
+
+
+/*Crie uma função que remove o primeiro elemento de um array.&*/
+const removeInicio = () => {
+    array.shift()
+    console.log(array)
+}
+
+/*Crie uma função que adiciona um elemento no início de um array*/
+ const adicionarInicio = (valor) => {
+    array.push(valor)
+    console.log(array) 
+}
+
+adicionarInicio(2)
+ 
+/*Crie uma função que remove o último elemento de um array.*/
+Function 
+
+
+
+
+
+/*Crie uma função que substitui um elemento em uma posição específica de
+um array.*/
+
+
+
+
+
+
+
+/*Crie uma função que adiciona múltiplos elementos ao final de um array.*/
+
+function adicionarFim =() =>{
+    array.push(”valor”)
+    console.log (array)
+
+}  
+
+
+
+
+/*Crie uma função que adiciona um elemento ao início do array e remove o
+último elemento.*/
+function adicionarFim =() =>{
+    array.push("")
+    function  adicionarInicio =>
+ array.unshift("")
+console.log (array)
+}
+
+
+/*Crie uma função que insere um elemento em uma posição específica de um
+array. O índice e o elemento a ser inserido devem ser passados como
+argumentos.*/
+
+git push
