d129f3e240eb505744c00dca7016ca58614cebb4
Yasmin Carolina <carolinazanini305@gmail.com>
2024-09-19 17:26:49 -0300

    aula quinta

--- aula7/index2.js
+++ aula7/index2.js
@@ -1,58 +1,46 @@
-const { select, input } = require('@inquirer/prompts');
+import {select,input,checkbox} from ("@inquirer/prompts")
+import chalk from 'chalk'
 
 const goal = {
-  value: 'Tomar 3L de água por dia',
-  checked: false
-};
+    value: "tomar 3L água por dia",
+    checked: false,
 
-const goals = [goal];
+}
+const goals= [goal]
+const registerGoal= async () => {
+    const goal = await input ({
+        message: "Digite a sua meta:",
 
-const registerGoal = async () => {
-  const goal = await input({
-    message: 'Digite a sua meta: '
-  });
-
-  if (goal.length === 0) {
-    console.log('A meta não pode ser vazia!');
-    return;
-  }
-
-  goals.push({ value: goal, checked: false });
-};
-
-const start = async () => {
-  while (true) {
-    const option = await select({
-      message: 'Menu > ',
-      choices: [
-        {
-          name: 'Cadastrar meta',
-          value: 'register'
-        },
-        {
-          name: 'Listar meta(s)',
-          value: 'list'
-        },
-        {
-          name: 'Sair',
-          value: 'out'
-        }
-      ]
-    });
-
-    switch (option) {
-      case 'register':
-        await registerGoal();
-        console.log(goals);
-        break;
-      case 'list':
-        console.log('Vamos listar sua(s) meta(s)!');
-        break;
-      case 'out':
-        console.log('Até a próxima!');
-        return;
+    })
+    if (goal.length ==0){
+        console.log("A meta não pode ser vazia!")
     }
-  }
-};
+    goals.push({value: goal, checked: false})
 
-start();
+}
+const listGoals = async () =>{
+    if (goals.length ==0){
+        console.log("Não existem metas cadastradas!")
+        return
+    }
+    const checkedGoals = await checkbox ({
+    message:
+    "Use as setas para mudar de meta, o espeço para marcar/desmarcar a meta e o enter para finalizar.",
+    choices: [...goals],
+    instructions: false,    
+    })
+    goals.forEach((goal) => {
+        goal.checked= false
+    })
+    if (checkedGoals==0){
+        console.log("Nenhuma meta foi selecionada!")
+        return
+}
+checkedGoals.forEach((checkedGoal)=>{
+    const goal= goals.find((selectedGoal)=>{
+        return selectedGoal.value == checkedGoal
+    })
+    goal.checked= true
+})
+console.log("meta(s) marcada(s)")
+}
